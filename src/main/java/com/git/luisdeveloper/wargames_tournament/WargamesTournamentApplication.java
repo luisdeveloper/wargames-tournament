5b1d5cdcc072ede1fff84f4bc84e96a211e8f8b7
Luis Criado San Miguel <psicolcsm@gmail.com>
2025-08-21 10:43:17 +0200

    first commit

---
+++ src/main/java/com/git/luisdeveloper/wargames_tournament/WargamesTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.git.luisdeveloper.wargames_tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WargamesTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WargamesTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/luisdeveloper/wargames_tournament/WargamesTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.luisdeveloper.wargames_tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WargamesTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
